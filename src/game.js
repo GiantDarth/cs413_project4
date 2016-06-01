3cfe354ff4f03e1c9a1f4b043c0aee7c1eb7d040
Christopher Robert Philabaum <chaos7theory@live.com>
2016-05-31 21:20:09 -0700

    Removed tileUtilities lib.

--- src/game.js
+++ src/game.js
@@ -212,8 +212,6 @@
         }
     }
 
-    const
-
     // Returns a random number between min (inclusive) and max (exclusive)
     function getRandomArbitrary(min, max) {
         return Math.random() * (max - min) + min;
